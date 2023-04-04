9c2e6d901f1199a0825fdd4bed0b5666bc13dbff
woowon11 <129824690+woowon11@users.noreply.github.com>
2023-04-04 14:41:29 +0900

    Update Keyboardinput.java

--- Keyboardinput.java
+++ Keyboardinput.java
@@ -2,7 +2,7 @@
  * 2023.03.14(화)
  * 세 개의 값을 입력 받아 첫 번째 값은 문장열, 두 번째 값은 정수값
  * 세 번째 값은 실수로 출력하는 프로그램
- * @author 차승헌
+ * 정우원
  */
 package chat02;
 
@@ -15,8 +15,8 @@ public class Keyboardinput {
 		String name = stdIn.next();//이름을 문자열로 입력
 		int i = stdIn.nextInt();//나이는 정수로 입력
 		double d=stdIn.nextDouble();//몸무게는 실수로 입력
-		System.out.println(name+"씨의 나이는"+i+"세이고");
-		System.out.println("몸무게는"+d+"kg입니다.");
+		System.out.println(name + "씨의 나이는" + i + "세이고");
+		System.out.println("몸무게는" + d + "kg입니다.");
 	}
 
 }
